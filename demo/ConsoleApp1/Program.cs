52fdf59c2ae931dfc406f63a2dbda678cd1be272
shaoxiangzhong <shao345678@qq.com>
2020-11-30 15:43:02 +0800

    console

---
+++ demo/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FTI(30));
+        }
+        static int FTI(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}
